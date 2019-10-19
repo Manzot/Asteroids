81ee2b9596c9e8fc42f2eb36b283d9d5cc44898c
Manjot <zotysingh@gmail.com>
2019-10-18 20:22:05 -0400

    Update Unit.cs

--- Assets/Resources/Scripts/Entities/Unit.cs
+++ Assets/Resources/Scripts/Entities/Unit.cs
@@ -2,13 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Unit : MonoBehaviour
+public class Unit : MonoBehaviour, IManager
 {
     Rigidbody2D rb;
+    public float speed;
 
+    public void Initialize()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void PhysicsRefresh()
+    {
+        
+    }
+
+    public void PostInitialize()
+    {
+        
+    }
+
+    public void Refresh()
+    {
+       
+    }
 
     void Move (Vector2 dir)
     {
-        dir = rb.velocity;
+        rb.velocity = dir.normalized * speed;
     }
 }
