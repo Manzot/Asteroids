989c362903fd9e5fafc0b2f589804070789d43d9
Karan Luther <35023765+lutherkaran@users.noreply.github.com>
2019-10-19 20:49:41 -0400

    Userinterface
    Basic Userinterface without InGame Pause.

---
+++ Assets/Resources/Scripts/Interface/MainMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public List<AudioSource> audioSources;
+
+    public void StartButtonPressed()
+    {
+        SceneManager.LoadScene("LevelOne");
+    }
+    public void SoundButtonPressed()
+    {
+       if( AudioListener.volume == 0)
+        {
+            AudioListener.volume = 1;
+        }
+        else
+        {
+            AudioListener.volume = 0;
+        }
+    }
+  
+
+    public void QuitButtonPressed() {
+        Application.Quit();
+    }
+}
